b50a5e1a7a17b6abd2c95d0d1fbb9e063229a3fb
Nick Newton <nickanewton@gmail.com>
2022-07-14 17:46:30 -0700

    Moved extendTheme.js into source directory.

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,9 +1,11 @@
 import { Box, Image } from "@chakra-ui/react"
+import brandLogo from '../assets/brand-logo.svg'
+
 function Banner() {
   return <>
           <header>
             <Box d="flex" alignItems="center" justifyContent="space-between" ml={6} mr={6}>
-              <Box><Image boxSize="90px" src="{brandLogo" /></Box>
+              <Box><Image boxSize="90px" src={brandLogo} alt="brand" /></Box>
               <Box></Box>
             </Box>
           </header>
